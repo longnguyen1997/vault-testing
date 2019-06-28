bbc97d51b26a60101d38f58c4023bdc5d710d251
Long <long@pf9-0113.local>
2019-06-28 13:30:28 -0700

    Refactored certs dictionary format and implemented CSR generation in Python using cryptography.x509

--- auth/certs.py
+++ auth/certs.py
@@ -1,5 +1,7 @@
 import hvac
+
 from generate_csr import *
+from os import getenv
 
 
 CLIENT_CERTS = {}
@@ -7,35 +9,46 @@ SERVER_CERTS = {}
 
 
 # The client cert etcdctl uses to talk to etcd.
-CLIENT_CERTS["etcdctl/etcd"]="--cn=etcdctl --cert_type=client"
+CLIENT_CERTS["etcdctl/etcd"] = {'cn': 'etcdctl', 'cert_type': 'client'}
 # The client cert flannel uses to talk to etcd.
-CLIENT_CERTS["flannel/etcd"]="--cn=flannel --cert_type=client"
+CLIENT_CERTS["flannel/etcd"] = {'cn': 'flannel', 'cert_type': 'client'}
 # The client cert kube-apiserver uses to talk to etcd.
-CLIENT_CERTS["apiserver/etcd"]="--cn=apiserver --cert_type=client"
+CLIENT_CERTS["apiserver/etcd"] = {'cn': 'apiserver', 'cert_type': 'client'}
 # The client cert kube-apiserver uses to talk to etcd.
-CLIENT_CERTS["kubelet/apiserver"]="--cn=kubelet --cert_type=client"
+CLIENT_CERTS["kubelet/apiserver"] = {'cn': 'kubelet', 'cert_type': 'client'}
 # The client cert kube-proxy uses to talk to kube-apiserver.
-CLIENT_CERTS["kube-proxy/apiserver"]="--cn=kube-proxy --cert_type=client"
+CLIENT_CERTS[
+    "kube-proxy/apiserver"] = {'cn': 'kube-proxy', 'cert_type': 'client'}
 # The client cert the admin user uses to talk to kube-apiserver.
-CLIENT_CERTS["admin"]="--cn=admin --cert_type=client --org=system:masters"
+CLIENT_CERTS["admin"] = {
+    'cn': 'admin',
+    'cert_type': 'client',
+    'org': 'system: masters'
+}
 # The client cert calico uses to talk to kube-apiserver.
-CLIENT_CERTS["calico/etcd"]="--cn=calico --cert_type=client"
+CLIENT_CERTS["calico/etcd"] = {'cn': 'calico', 'cert_type': 'client'}
 # The client cert the kube api aggregator uses to talk to kube-apiserver.
-CLIENT_CERTS["aggregator"]="--cn=aggregator --cert_type=client"
+CLIENT_CERTS["aggregator"] = {'cn': 'aggregator', 'cert_type': 'client'}
 
 
 # The server cert etcd presents to clients.
-SERVER_CERTS["etcd/client"]="--cn=etcd --cert_type=server --sans=${trimmed_etcd_sans} "
+SERVER_CERTS["etcd/client"] = {'cn': 'etcd',
+                               'cert_type': 'server', 'sans': getenv('trimmed_etcd_sans')}
 # The server cert that etcd presents to peers.
-SERVER_CERTS["etcd/peer"]="--cn=etcd --cert_type=server --sans=${trimmed_etcd_sans} "
+SERVER_CERTS["etcd/peer"] = {'cn': 'etcd',
+                             'cert_type': 'server', 'sans': getenv('trimmed_etcd_sans')}
 # The server cert kube-apiserver presents to clients.
-SERVER_CERTS["apiserver"]="--cn=apiserver --cert_type=server --sans=${trimmed_apiserver_sans} --needs_svcacctkey=true "
+SERVER_CERTS["apiserver"] = {'cn': 'apiserver', 'cert_type': 'server', 'sans': getenv('trimmed_apiserver_sans'),
+                             'needs_svcacctkey': True}
 # The server cert kubelet presents to kube-apiserver (not used currently).
-SERVER_CERTS["kubelet/server"]="--cn=kubelet --cert_type=server --sans=${trimmed_kubelet_sans} "
+SERVER_CERTS["kubelet/server"] = {'cn': 'kubelet',
+                                  'cert_type': 'server', 'sans': getenv('trimmed_kubelet_sans')}
 # The server cert the authn webhook presents to kube-apiserver.
-SERVER_CERTS["authn_webhook"]="--cn=admin --cert_type=server --sans=${trimmed_auth_webhook}"
+SERVER_CERTS["authn_webhook"] = {
+    'cn': 'admin', 'cert_type': 'server', 'sans': getenv('trimmed_auth_webhook')}
 # The server cert the kubernetes dashboard presents to users.
-SERVER_CERTS["dashboard"]="--cn=dashboard  --cert_type=server --sans=${trimmed_dashboard_sans}"
+SERVER_CERTS["dashboard"] = {
+    'cn': 'dashboard ', 'cert_type': 'server', 'sans': getenv('trimmed_dashboard_sans')}
 
 print(CLIENT_CERTS)
 print(SERVER_CERTS)
--- auth/generate_csr.py
+++ auth/generate_csr.py
@@ -2,32 +2,74 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives.serialization import Encoding
+from cryptography.x509 import DNSName
+from cryptography.x509 import IPAddress
+from cryptography.x509.oid import NameOID
+from ipaddress import IPv4Address
 
-def generate_csr():
-  '''
-  Returns an X509 CSR in 
-  cryptography.hazmat.backends.openssl.x509._CertificateSigningRequest
-  format. See documentation for more details on how to sign.
-  '''
-  private_key = rsa.generate_private_key(
-                     public_exponent=65537,
-                                  key_size=2048,
-                                               backend=default_backend()
-                                                        )
-  builder = x509.CertificateSigningRequestBuilder()
-  builder = builder.subject_name(x509.Name([
-               x509.NameAttribute(NameOID.COMMON_NAME, u'cryptography.io'),
-                    ]))
-  builder = builder.add_extension(
-                       x509.BasicConstraints(ca=False, path_length=None), critical=True,
-                                )
-  request = builder.sign(
-                       private_key, hashes.SHA256(), default_backend()
-                                )
-  print(request.public_bytes(Encoding.PEM))
-  isinstance(request, x509.CertificateSigningRequest)
-  return request
 
-generate_csr()
+def generate_csr(cn, csr_path, private_key_path, pki_dir, sans={}, org=None):
+    '''
+    Returns an X509 CSR in 
+    cryptography.hazmat.backends.openssl.x509._CertificateSigningRequest
+    format. See documentation for more details on how to sign.
+
+    cn (str): Common name.
+    csr_path (str): Path to store CSR file.
+    private_key_path (str): Path to store private key.
+    pki_dir (str): Path for PKI directory.
+    sans (dict: str -> str): Maps {types, ex. 'IP' -> values, ex. '10.5.1.16'}.
+    '''
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    builder = x509.CertificateSigningRequestBuilder()
+
+    # Add the basic attributes. Includes CN.
+    builder = builder.subject_name(x509.Name([
+        # Provide various details about who we are.
+        x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, u"Sunnyvale"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"Platform9 Systems"),
+        x509.NameAttribute(NameOID.COMMON_NAME, cn),
+    ]))
+
+    # Convert the SANs to Python objects.
+    sans_converted = []
+    for san in sans:
+        if san == 'IP':
+            ip_addresses = sans[san]
+            for ip_address in ip_addresses:
+                sans_converted.append(IPAddress(IPv4Address(ip_address)))
+        if san == 'DNS':
+            dns_names = sans[san]
+            for dns_name in dns_names:
+                sans_converted.append(DNSName(dns_name))
+
+    # Add the SANs to the CSR information.
+    builder = builder.add_extension(
+        x509.SubjectAlternativeName(sans_converted),
+        critical=False
+    )
+
+    builder = builder.add_extension(
+        x509.BasicConstraints(ca=False, path_length=None), critical=True,
+    )
+
+    # Sign the CSR with our private key.
+    request = builder.sign(
+        private_key, hashes.SHA256(), default_backend()
+    )
+
+    return request
+
+# Example CSR generation call.
+request = generate_csr('etcd', '/tmp/authbs-certs.wTmw/etcd/peer/request.csr', '/tmp/authbs-certs.wTmw/etcd/peer/request.key',
+                       '/tmp/authbs-certs.wTmw/etcd/peer/pki', {'IP': ['127.0.0.1', '10.5.1.16', '10.5.1.16', '10.5.1.16']})
+# Print the CSR, PEM-encoded.
+print(request.public_bytes(Encoding.PEM))
